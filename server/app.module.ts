c7ea46defa085cb7da42189d7cc807624b86d5fb
Maksim Zakharov <max89701@gmail.com>
2021-07-07 15:20:31 +0300

    change typeorm config

--- server/app.module.ts
+++ server/app.module.ts
@@ -13,20 +13,33 @@ import { LabEntity } from './entities/lab.entity';
 import { ClinicStaffEntity } from './entities/clinic-staff.entity';
 import { LabStaffEntity } from './entities/lab-staff.entity';
 
+const host =
+  process.env.RDS_HOSTNAME || process.env.POSTGRES_HOST || 'localhost';
+const port = +process.env.RDS_PORT || 5432;
+const username =
+  process.env.RDS_USERNAME || process.env.POSTGRES_USER || 'postgres';
+const password =
+  process.env.RDS_PASSWORD || process.env.POSTGRES_PASSWORD || 'password';
+const database = process.env.RDS_DB_NAME || process.env.POSTGRES_DB ||  'fleming-clinic';
+const url =
+  process.env.DATABASE_URL ||
+  `postgresql://${username}:${password}@${host}/${database}`;
+
 @Module({
   imports: [
     ConfigModule.forRoot(), // load .env file
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.RDS_HOSTNAME || process.env.POSTGRES_HOST || 'localhost',
-      port: +process.env.RDS_PORT || 5432,
-      username: process.env.RDS_USERNAME || process.env.POSTGRES_USER || 'postgres',
-      password: process.env.RDS_PASSWORD || process.env.POSTGRES_PASSWORD || 'password',
-      database: process.env.RDS_DB_NAME || process.env.POSTGRES_DB || 'fleming-clinic',
+      url,
+      host,
+      port,
+      username,
+      password,
+      database,
       autoLoadEntities: true,
       synchronize: true,
       ssl:
-        process.env.NODE_ENV === 'production'
+        process.env.DATABASE_URL
           ? { rejectUnauthorized: false }
           : false,
     }),
